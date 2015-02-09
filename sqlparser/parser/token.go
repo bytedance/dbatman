0852e787c676ca46f43583ff2cf6591fbce419c1
Wang Jing <wangjild@gmail.com>
2015-02-09 22:26:28 +0800

    add token name functions

---
+++ sqlparser/parser/token.go
@@ -0,0 +1,13 @@
+package parser
+
+import (
+	"fmt"
+)
+
+func TokenName(tok int) string {
+	if (tok-ABORT_SYM) < 0 || (tok-ABORT_SYM) > len(MySQLToknames) {
+		return fmt.Sprintf("Unknown Token:%d", tok)
+	}
+
+	return MySQLToknames[tok-ABORT_SYM]
+}
