0279e9140a9acb3ac3a3915fb031769d587f0322
Wang Jing <wangjild@gmail.com>
2016-06-02 15:00:26 +0800

    fix codecov

--- test.sh
+++ test.sh
@@ -6,20 +6,14 @@ echo "" > coverage.txt
 for d in $(find ./* -maxdepth 3 -type d); do
     if ls $d/*.go &> /dev/null; then
         IMPORT_LIST=`go list -f "{{.Imports}}" $d | sed -e "s/\[//g" | sed -e "s/\]//g"`
-        BYTEDANCE_PKG=''
+        BYTEDANCE_PKG=`go list $d`
 		for i in ${IMPORT_LIST[@]}; do
             if [[ ${i} == github\.com\/bytedance\/dbatman* ]]; then
 				BYTEDANCE_PKG=${BYTEDANCE_PKG},${i}
             fi
         done
 		
-		BYTEDANCE_PKG=`echo ${BYTEDANCE_PKG} | sed -e "s/^,//g"`
-
-		if [[ ${BYTEDANCE_PKG} == '' ]]; then
-			go test -coverprofile=profile.out -covermode=atomic $d
-		else
-        		go test -coverprofile=profile.out -covermode=atomic $d -coverpkg=${BYTEDANCE_PKG}
-        	fi
+        go test -coverprofile=profile.out -covermode=atomic $d -coverpkg=${BYTEDANCE_PKG}
 			
 		if [ -f profile.out ]; then
             cat profile.out >> coverage.txt
